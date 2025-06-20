1ee40fb595abc47f2127f223a2452489f65c1eda
Lemonfloe <lexi.almaga@gmail.com>
2025-06-20 06:38:45 -0700

    fixed navigation fade

--- main.js
+++ main.js
@@ -44,34 +44,33 @@ window.addEventListener("DOMContentLoaded", changeContent('homepage.html'), (cur
 
 document.querySelectorAll(".nav__button").forEach(function (button, index) {
     button.addEventListener("click", async function () {
-        currentPageIndex = index;
-        const contentURL = pageFiles[currentPageIndex];
-        await fadeOut(pageContent);
-        await changeContent(contentURL);
-        fadeIn(pageContent);
+
+        if (currentPage != index) {
+            currentPageIndex = index;
+            const contentURL = pageFiles[currentPageIndex];
+            await fadeOut(pageContent);
+            await changeContent(contentURL);
+            await fadeIn(pageContent);
+        } else {
+            console.log('already here!');
+        }
     });
 });
 
 function changeContent(contentURL) {
-    if (currentPage != currentPageIndex) {
-        fetch(contentURL)
-            .then(res => {
-                return res.text();
-            })
-            .then(cont => {
-                var divPlaceholder = document.getElementById('pageContent');
-                document.getElementById('pageContent').innerHTML = "";
-                divPlaceholder.insertAdjacentHTML("afterbegin", cont);
-                setupAudioMotionAnalyzer();
-                if (contentURL === 'creations.html') {
-                    setupCreationsAudio();
-                }
-            })
-            .then(currentPage = currentPageIndex);
-    } else {
-        console.log('already here!');
-    }
-};
+    return fetch(contentURL)
+        .then(res => res.text())
+        .then(cont => {
+            var divPlaceholder = document.getElementById('pageContent');
+            divPlaceholder.innerHTML = "";
+            divPlaceholder.insertAdjacentHTML("afterbegin", cont);
+            setupAudioMotionAnalyzer();
+            if (contentURL === 'creations.html') {
+                setupCreationsAudio();
+            }
+            currentPage = currentPageIndex;
+        })
+}
 
 function fadeOut(element) {
     return new Promise((resolve) => {
@@ -176,31 +175,31 @@ function setupCreationsAudio() {
     if (!audioElement) return;
 
     const audioFiles = [
-    "https://dl.dropbox.com/scl/fi/3kni8e09vbao66f1bn7ct/floe-rainbowpotion-demo.mp3?rlkey=2ui12h9hos3e7ema81n9nqf6k",
-    "https://dl.dropbox.com/scl/fi/5w5846eaar79yc9q4yv3s/floe-thrash-demo.mp3?rlkey=g13zn9599x04md50oocl3axx7",
-    "https://dl.dropbox.com/scl/fi/1t1avibsj3p5u2j41o401/floe-crimson-demo.mp3?rlkey=ryl3yojadddbk26dii74e5729",
-    "https://dl.dropbox.com/scl/fi/hp80vg637j9rwdesll6vx/floe-splitcircuit-demo.mp3?rlkey=geikgnz3b1hg5j7u6bn8uldz6",
-    "https://dl.dropbox.com/scl/fi/tzpv7xu268rhql1ao8gne/floe-senseless.mp3?rlkey=2ufonpvudfvocxrzdvtym7xgz&st=47czh5yw",
-    "https://dl.dropbox.com/scl/fi/vni60xa1gdjmq6kjgl6zy/floe-chameleon-demo.mp3?rlkey=q7vhwaoiuby77es5fdjcfs79s&st=csb8ad2r",
-    "https://dl.dropbox.com/scl/fi/tf0a3w2ub0w6gqqglj7kz/MY-BLOOD-IS-LITERALLY-SUGAR.mp3?rlkey=1eq910phzvax4nhdx6efhsst6"
-];
+        "https://dl.dropbox.com/scl/fi/3kni8e09vbao66f1bn7ct/floe-rainbowpotion-demo.mp3?rlkey=2ui12h9hos3e7ema81n9nqf6k",
+        "https://dl.dropbox.com/scl/fi/5w5846eaar79yc9q4yv3s/floe-thrash-demo.mp3?rlkey=g13zn9599x04md50oocl3axx7",
+        "https://dl.dropbox.com/scl/fi/1t1avibsj3p5u2j41o401/floe-crimson-demo.mp3?rlkey=ryl3yojadddbk26dii74e5729",
+        "https://dl.dropbox.com/scl/fi/hp80vg637j9rwdesll6vx/floe-splitcircuit-demo.mp3?rlkey=geikgnz3b1hg5j7u6bn8uldz6",
+        "https://dl.dropbox.com/scl/fi/tzpv7xu268rhql1ao8gne/floe-senseless.mp3?rlkey=2ufonpvudfvocxrzdvtym7xgz&st=47czh5yw",
+        "https://dl.dropbox.com/scl/fi/vni60xa1gdjmq6kjgl6zy/floe-chameleon-demo.mp3?rlkey=q7vhwaoiuby77es5fdjcfs79s&st=csb8ad2r",
+        "https://dl.dropbox.com/scl/fi/tf0a3w2ub0w6gqqglj7kz/MY-BLOOD-IS-LITERALLY-SUGAR.mp3?rlkey=1eq910phzvax4nhdx6efhsst6"
+    ];
 
-let currentAudioIndex = 0;
+    let currentAudioIndex = 0;
 
-function changeAudio(index) {
-    currentAudioIndex = index;
-    audioElement.src = audioFiles[index];
-    audioElement.load();
-    audioElement.play();
-}
+    function changeAudio(index) {
+        currentAudioIndex = index;
+        audioElement.src = audioFiles[index];
+        audioElement.load();
+        audioElement.play();
+    }
 
-document.querySelectorAll(".myMusic__button").forEach(function (button, index) {
-    button.addEventListener("click", function () {
-        changeAudio(index);
+    document.querySelectorAll(".myMusic__button").forEach(function (button, index) {
+        button.addEventListener("click", function () {
+            changeAudio(index);
+        });
     });
-});
 
-audioElement.addEventListener("ended", function () {
+    audioElement.addEventListener("ended", function () {
         let nextIndex = (currentAudioIndex + 1) % audioFiles.length;
         changeAudio(nextIndex);
     });
