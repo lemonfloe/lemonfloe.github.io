24b794cfdda5af935d9b9385acc482c49cc98857
Lemonfloe <lexi.almaga@gmail.com>
2025-06-20 14:19:51 -0700

    folder structure updates, not_found html added

--- main.js
+++ main.js
@@ -207,3 +207,21 @@ function setupCreationsAudio() {
 }
 
 // #endregion
+
+// #region wip autoplayer
+
+// var button = document.getElementById("button");
+// var audio = document.getElementById("player");
+      
+//       button.addEventListener('click', function(){      
+//         if(audio.paused){
+//         audio.play();
+//         button.innerHTML = '<i class="fa fa-pause"></i>';
+          
+//         } else {
+//         audio.pause();
+//         button.innerHTML = '<i class="fa fa-play"></i>';
+//         }
+//       });
+
+// #endregion
