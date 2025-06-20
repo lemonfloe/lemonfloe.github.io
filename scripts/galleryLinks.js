21381f877b9d381d925a8cb3100364da1163ebe2
Lemonfloe <lexi.almaga@gmail.com>
2025-06-20 15:01:05 -0700

    gallery generation scripts

---
+++ scripts/galleryLinks.js
@@ -0,0 +1,8 @@
+const galleryLinks = [
+  "/assets/gallery/blood.jpg",
+  "/assets/gallery/candy.jpg",
+  "/assets/gallery/eugene.jpg",
+  "/assets/gallery/feesh.jpg",
+  "/assets/gallery/ralsei.jpg",
+  "/assets/gallery/ttekocto.jpg"
+];
